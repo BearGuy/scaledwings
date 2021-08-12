b0c2d827d0ec6b357918bc52e180cec6f1d13191
BearGuy <stephen@peterkins.ca>
2021-08-12 07:45:55 -0400

    feature: use nextjs image to optimize loading of photos in gallery

--- components/Content/index.js
+++ components/Content/index.js
@@ -1,7 +1,12 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from './content.module.css'
 
+import useDimensions from "react-cool-dimensions";
+
 export const Content = ({ posts }) => {
+  const { observe, width } = useDimensions();
+
   return (
     <div className={`p-5 md:overflow-y-scroll h-screen`}>
       <div className={styles.masonry}>
@@ -10,7 +15,17 @@ export const Content = ({ posts }) => {
             return (
               <Link idx={idx} key={idx} href={`/posts/${post.id}`}>
                 <div className={styles.item}>
-                  <img src={post.url} />
+                  {/* // <img src={post.url} /> */}
+                  <div ref={observe}>
+                    <Image
+                      src={post.url}
+                      height={16}
+                      width={9}
+                      layout="responsive"
+                      quality={65}
+                      sizes={width !== undefined ? `${Math.round(width)}px` : '100vw'}
+                    />
+                  </div>
                 </div>
               </Link>
             )

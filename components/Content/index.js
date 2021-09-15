c49ea41f1688812a757f2cc2bbc538296495b10b
BearGuy <stephen@peterkins.ca>
2021-09-15 11:08:51 -0400

    feature: next image correct implementation in gallery

--- components/Content/index.js
+++ components/Content/index.js
@@ -2,11 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from './content.module.css'
 
-import useDimensions from "react-cool-dimensions";
-
 export const Content = ({ posts }) => {
-  const { observe, width } = useDimensions();
-
   return (
     <div className={`p-5 md:overflow-y-scroll h-screen`}>
       <div className={styles.masonry}>
@@ -15,15 +11,11 @@ export const Content = ({ posts }) => {
             return (
               <Link idx={idx} key={idx} href={`/posts/${post.id}`}>
                 <div className={styles.item}>
-                  {/* // <img src={post.url} /> */}
-                  <div ref={observe}>
+                  <div className={`${styles.unset_img}`}>
                     <Image
                       src={post.url}
-                      height={16}
-                      width={9}
-                      layout="responsive"
-                      quality={65}
-                      sizes={width !== undefined ? `${Math.round(width)}px` : '100vw'}
+                      className={`${styles.custom_img}`}
+                      layout="fill"
                     />
                   </div>
                 </div>

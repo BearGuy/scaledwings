cbe12a7e20bd6c6332c1ab37371219febe5831a4
BearGuy <stephen@peterkins.ca>
2021-09-15 11:31:28 -0400

    fix: significant decrease in image quality on initial load

--- components/Content/index.js
+++ components/Content/index.js
@@ -16,6 +16,7 @@ export const Content = ({ posts }) => {
                       src={post.url}
                       className={`${styles.custom_img}`}
                       layout="fill"
+                      quality="5"
                     />
                   </div>
                 </div>
